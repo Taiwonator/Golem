3f4f6b49d2492d483cece759fa4e116e303b2da9
Michael Taiwo <22432591+Taiwonator@users.noreply.github.com>
2022-03-18 22:08:22 +0000

    fix(strapi: gitignore): removes database.js file

---
+++ strapi/config/database.js
@@ -0,0 +1,12 @@
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 3001),
+      database: env('DATABASE_NAME', 'Blog'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', 'Qualitycake7ql'),
+    },
+  },
+});
